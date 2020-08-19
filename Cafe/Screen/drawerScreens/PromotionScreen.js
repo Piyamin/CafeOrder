c783e840350b8cd3f21fc394bafb860f593fc40b
Piyamin <piyaminjaima42@gmail.com>
2020-08-20 05:13:32 +0700

    Promotion

--- Cafe/Screen/drawerScreens/PromotionScreen.js
+++ Cafe/Screen/drawerScreens/PromotionScreen.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import { StyleSheet, Button, View, TouchableOpacity, Text, Alert } from 'react-native';
+import { StyleSheet, Button, View, TouchableOpacity, Text, Alert,ScrollView } from 'react-native';
 
 const Separator = () => (
   <View style={styles.separator} />
 );
 
 const Promotion = ({ navigation }) => (
-  <TouchableOpacity
-            style={styles.boxDevice}
-            onPress={() => navigation.navigate('DetailScreen')}>
-            <View style={{ margin: 10 }}>
-              <Text style={styles.textPublic}></Text>
-              <Text style={styles.textPublic}>Promotion</Text>
-            </View>
-  </TouchableOpacity>
-
+  <View>
+    <ScrollView>
+      <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+    <TouchableOpacity
+              style={styles.boxDevice}
+              onPress={() => navigation.navigate('DetailScreen')}>
+              <View style={{ margin: 10 }}>
+                <Text style={styles.textPublic}></Text>
+                <Text style={styles.textPublic}>Promotion</Text>
+              </View>
+    </TouchableOpacity>
+  </ScrollView>
+  </View>
 );
 const styles = StyleSheet.create({
   boxDevice: {
     backgroundColor: 'rgba(0,0,0,0.1)',
     width: '100%',
-    height: 100,
+    height: 200,
     marginTop: 16,
   },
   // stretch: {
